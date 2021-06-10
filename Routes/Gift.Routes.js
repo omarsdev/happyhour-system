9a7a4e566c9cac8ca183ff9dfceab89e53b22f3c
Omar Sukarieh <omarsukarieh99@gmail.com>
2021-06-10 17:06:20 +0300

    import casher controller in git route

--- Routes/Gift.Routes.js
+++ Routes/Gift.Routes.js
@@ -6,13 +6,19 @@ const {
   getAllGifts,
   createNewBrandGift,
   getAllGift,
+  //chacher
+  checkGiftCode,
+  confirmGiftCode,
 } = require("../Controller/Gift.Controller");
 
 const router = express.Router();
 
 router.route("/").get(protect, getAllGifts);
+//cacher
+router.route("/check").get(protect, checkGiftCode).post(confirmGiftCode);
 router
   .route("/:brandid")
+  // .get(protect, checkGiftCode)
   .get(protect, getAllGift)
   .post(protect, authorize("client"), createNewBrandGift);
 
